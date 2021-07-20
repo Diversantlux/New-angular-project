2d33f3c77d04fbeeb0948a7dda6ab20043215cdc
Diversantlux <diversantlux@gmail.com>
2021-07-21 02:12:17 +0300

    Add new folder and update pages

---
+++ src/app/pages/page-all-scoring/page-all-scoring.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-page-all-scoring',
+    templateUrl: './page-all-scoring.component.html',
+    styleUrls: ['./page-all-scoring.component.css']
+  })
+
+  export class ComponentPageAllScoringComponent {
+
+}
---
+++ src/app/pages/page-new-scoring/page-new-scoring.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-page-new-scoring',
+    templateUrl: './page-new-scoring.component.html',
+    styleUrls: ['./page-new-scoring.component.css']
+  })
+
+  export class ComponentPageNewScoringComponent {
+
+}
